13cf6fb29de356e08f6a3bda9e90094f9673f7cb
AndersonRochaJC <jmcreggae@gmail.com>
2021-08-27 20:06:19 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;            
+              Console.ForegroundColor = ConsoleColor.Black ;
+            Console.WriteLine("YESHUA");
+             Console.WriteLine('\n');
+             Console.BackgroundColor = ConsoleColor.Green;
+             Console.WriteLine("Te chamam de Deus e de Senhor, Te chamam de Rei e Salvador");
+             Console.WriteLine("Mas eu me atrevo a te chamar De meu amor");
+             Console.WriteLine('\n');
+             Console.WriteLine("Te chamam de Deus e de Senhor, Te chamam de Rei e Salvador");
+            Console.WriteLine("Mas eu me atrevo a te chamar De meu amor");
+            Console.WriteLine('\n');
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Yeshua, Yeshua");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Tu és tão lindo,");
+            Console.WriteLine("Que eu nem sei Expressar");
+            Console.WriteLine('\n');
+            Console.BackgroundColor = ConsoleColor.Red;
+             Console.WriteLine("Yeshua, Yeshua");
+             Console.BackgroundColor = ConsoleColor.Green;
+             Console.WriteLine("Tu és tão lindo");
+             Console.WriteLine("Que eu nem sei Expressar");
+             Console.WriteLine('\n');
+              Console.BackgroundColor = ConsoleColor.Red;
+             Console.WriteLine("Yeshua, Yeshua");
+             Console.WriteLine("Yeshua, Yeshua");
+             Console.BackgroundColor = ConsoleColor.Black;
+             Console.ForegroundColor = ConsoleColor.White;
+           
+
+
+
+
+
+        }
+    }
+}
