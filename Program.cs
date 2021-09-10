196fc77201786cedda868cf0d2e1474b5a5a64ff
Diego Neri <diego@diegoneri.com>
2021-09-10 18:06:28 -0300

    Ajusta indentação e reset de cores

--- Program.cs
+++ Program.cs
@@ -6,15 +6,15 @@ namespace Musica
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Yellow;            
-              Console.ForegroundColor = ConsoleColor.Black ;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine("YESHUA");
-             Console.WriteLine('\n');
-             Console.BackgroundColor = ConsoleColor.Green;
-             Console.WriteLine("Te chamam de Deus e de Senhor, Te chamam de Rei e Salvador");
-             Console.WriteLine("Mas eu me atrevo a te chamar De meu amor");
-             Console.WriteLine('\n');
-             Console.WriteLine("Te chamam de Deus e de Senhor, Te chamam de Rei e Salvador");
+            Console.WriteLine('\n');
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Te chamam de Deus e de Senhor, Te chamam de Rei e Salvador");
+            Console.WriteLine("Mas eu me atrevo a te chamar De meu amor");
+            Console.WriteLine('\n');
+            Console.WriteLine("Te chamam de Deus e de Senhor, Te chamam de Rei e Salvador");
             Console.WriteLine("Mas eu me atrevo a te chamar De meu amor");
             Console.WriteLine('\n');
             Console.BackgroundColor = ConsoleColor.Red;
@@ -24,22 +24,15 @@ namespace Musica
             Console.WriteLine("Que eu nem sei Expressar");
             Console.WriteLine('\n');
             Console.BackgroundColor = ConsoleColor.Red;
-             Console.WriteLine("Yeshua, Yeshua");
-             Console.BackgroundColor = ConsoleColor.Green;
-             Console.WriteLine("Tu és tão lindo");
-             Console.WriteLine("Que eu nem sei Expressar");
-             Console.WriteLine('\n');
-              Console.BackgroundColor = ConsoleColor.Red;
-             Console.WriteLine("Yeshua, Yeshua");
-             Console.WriteLine("Yeshua, Yeshua");
-             Console.BackgroundColor = ConsoleColor.Black;
-             Console.ForegroundColor = ConsoleColor.White;
-           
-
-
-
-
-
+            Console.WriteLine("Yeshua, Yeshua");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Tu és tão lindo");
+            Console.WriteLine("Que eu nem sei Expressar");
+            Console.WriteLine('\n');
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Yeshua, Yeshua");
+            Console.WriteLine("Yeshua, Yeshua");
+            Console.ResetColor();
         }
     }
 }
